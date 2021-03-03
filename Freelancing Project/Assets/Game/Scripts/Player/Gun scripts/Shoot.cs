2ad6740d469582baba851b43b321decbb9a79cf5
Jari-van-der-Kolk <58220294+Jari-van-der-Kolk@users.noreply.github.com>
2021-03-03 11:54:11 +0100

    update

--- Freelancing Project/Assets/Game/Scripts/Player/Gun scripts/Shoot.cs
+++ Freelancing Project/Assets/Game/Scripts/Player/Gun scripts/Shoot.cs
@@ -7,10 +7,11 @@ public class Shoot : MonoBehaviour
 
     [SerializeField] private FloatReference damage;
     [SerializeField] private FloatReference magezineSize;
-    [SerializeField] private FloatReference currentAmmo;
+    [SerializeField] private FloatReference currentAmmoInMag;
+    [SerializeField] private FloatReference amountOfBullets;
     [SerializeField] private FloatReference shootingSpeed;
     private Timer timer = new Timer();
-    private Ammo ammo;
+    private magazineBehaviour ammo;
 
     private IRayProvider rayProvider;
     private ISelector selector;
@@ -19,27 +20,37 @@ public class Shoot : MonoBehaviour
 
     void Start()
     {
-        ammo = new Ammo(magezineSize.Value, currentAmmo.Value);
+        ammo = new magazineBehaviour(magezineSize.Value, currentAmmoInMag.Value, amountOfBullets.Value);
         rayProvider = GetComponent<IRayProvider>(); //dit called het midden van het scherm
         selector = GetComponent<ISelector>(); // maakt de ray die ervoor zorgt dat je de enemy raakt
     }
 
     void Update()
     {
+        Shooting();
+    }
+    
+    void Shooting()
+    {
         timer.GetDeltaTime(Time.deltaTime);
         timer.TimeMultiplication(shootingSpeed.Value);
 
         if (Input.GetMouseButton(0) && timer.time > 1f && ammo.HasAmmoInMag())
         {
+            ammo.Shot();
+            Debug.Log(ammo.HasAmmoInMag());
+
             selector.Check(rayProvider.CreateRay());
 
-            if(selector.GetSelection() != null)
-            selector.GetSelection().GetComponent<Idamageable>().GetDamaged(damage.Value);
-            
+            if (selector.GetSelection() != null)
+                selector.GetSelection().GetComponent<Idamageable>().GetDamaged(damage.Value);
+
             Debug.Log(name + "has shot");
             timer.ResetTime(0);
         }
-
-
+        else if (!ammo.HasAmmoInMag())
+        {
+            return;
+        }
     }
 }
